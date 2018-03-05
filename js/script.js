16dacaa0e19d0400d90179dfb2fa76e35888cbee
Dair Baidauletov <dair.baidauletov@gmail.com>
2018-03-05 14:37:23 +0200

    RSS minor changes

--- js/script.js
+++ js/script.js
@@ -5,17 +5,32 @@ $(document).ready(function () {
     .then(response => response.text())
     .then(contents => {
       var xmlDoc = $.parseXML(contents);
-      console.log(xmlDoc)
       var items = $("item", xmlDoc);
-      console.log(items)
       $.each( items, function( index, current ) {
         if (index == 3) {
           return false;
         }
-        $( ".card-title" ).eq( index )
-          .html(current.getElementsByTagName("pubDate")[0].childNodes[0].nodeValue.substring(5, 17));
-        $( ".card-text" ).eq( index )
-          .html(current.getElementsByTagName("title")[0].childNodes[0].nodeValue);
+        // Setting date
+        var date = new Date(current.getElementsByTagName("pubDate")[0].childNodes[0].nodeValue),
+        year      = date.getFullYear(),
+        month   = date.getMonth() < 10 ? '0' + date.getMonth() : date.getMonth() + 1, // 0 to 1
+        day     = date.getDate()  < 10 ? '0' + date.getDate()  : date.getDate(),
+        newDate = day + '.' + month + '.' + year;
+        $( ".news-card > a > .card-title" ).eq( index )
+          .html(newDate);
+          console.log(newDate)
+
+        // Setting title
+        var title = current.getElementsByTagName("title")[0].childNodes[0].nodeValue;
+        title = title.replace(/\[.*?\]/g, ""); // Remove extra tag
+        $( ".news-card > a >.card-text" ).eq( index )
+          .html(title);
+          console.log(title)
+
+        //Setting link
+        var link  = current.getElementsByTagName("link")[0].childNodes[0].nodeValue + "";
+        $( ".news-card > a" ).eq( index )
+          .attr("href", link);
       })
     })
     .catch(() => console.log("Can’t access " + url + " response. Blocked by browser?"))
